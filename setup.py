0d0a951255946aa33420b967afc779dbcbd02ee5
kdb424 <kdb424@gamil.com>
2016-06-18 16:29:04 -0500

    Actually push setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='haruhichan_rss',
+    version='.1',
+    packages=[''],
+    url='https://github.com/kdb424/haruhichan_rss',
+    license='MIT',
+    author='kdb424',
+    author_email='kdb424@gmail.com',
+    description='Downloads files from Haruhichan RSS feed', requires=['feedparser']
+)
