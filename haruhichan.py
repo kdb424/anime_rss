a69b00e73125efa0add66b8258ecdafc0e85eb7b
kdb424 <kdb424@gamil.com>
2016-06-18 12:25:58 -0500

    Initial commit

---
+++ haruhichan.py
@@ -0,0 +1,59 @@
+
+'''
+    Downloads titles from haruhichan.com automatically from the RSS feed
+'''
+
+from os.path import expanduser
+import urllib.request
+import argparse
+import feedparser
+import yaml
+
+RSS_URL = "http://haruhichan.com/feed/feed.php?mode=rss"
+
+FEED = feedparser.parse(RSS_URL)
+
+HOME = expanduser("~")
+CONFIG_FILE = HOME + "/.config/haruhichan.cfg"
+DOWNLOAD_DIR = HOME + "/Downloads/"
+
+
+PARSER = argparse.ArgumentParser(description='Does stuff')
+PARSER.add_argument('-c', '--config', help='Configuration file location',
+                    required=False)
+PARSER.add_argument('-d', '--download', help='Download directory',
+                    required=False)
+ARGS = vars(PARSER.parse_args())
+
+if ARGS['config'] is not None:
+    CONFIG_FILE = ARGS['config']
+
+if ARGS['download'] is not None:
+    DOWNLOAD_DIR = ARGS['download']
+
+
+def get_conf():
+    '''
+        Returns a list of titles from config file
+    '''
+    with open(CONFIG_FILE, 'r') as config:
+        cfg = yaml.load(config)
+        return cfg
+
+
+YAML_CFG = get_conf()
+TITLE = YAML_CFG["title"]
+QUALITY = YAML_CFG["quality"]
+SUBBER = YAML_CFG["subber"]
+for post in FEED.entries:
+    for title in TITLE:
+        if str(title) in post.title:
+            for sub in SUBBER:
+                if str(sub) in post.title:
+                    for qul in QUALITY:
+                        if str(qul) in post.title:
+                            urllib.request.urlretrieve(post.link,
+                                                       DOWNLOAD_DIR +
+                                                       post.title +
+                                                       ".torrent")
+                            print(post.title + ": " + post.link)
